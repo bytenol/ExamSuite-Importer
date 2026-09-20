e36ac29c56c03b0a5212503b24bf72aec7462104
Bytenol <mustaphaibrahim4099@gmail.com>
2026-09-20 13:48:57 +0100

    introduced pytest

--- test/question_test.py
+++
@@ -1,9 +0,0 @@
-from examsuite_importer.question.QuestionParser import QuestionParser
-
-
-
-if __name__ == "__main__":
-    parser = QuestionParser("/home/user/Documents/Programming/ByteNol/ExamSuite-Importer/scratch/question_test.docx")
-    result = parser.parse()
-
-    print(result)
